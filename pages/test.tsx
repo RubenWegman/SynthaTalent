5b23232151b0eb31c4da7264b7287d4c1826242c
RubenWegman <153371271+RubenWegman@users.noreply.github.com>
2025-03-30 09:13:45 -0400

    Create test.tsx

---
+++ pages/test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>TESTPAGE</h1>
+      <p>If you see this, routing works!</p>
+    </main>
+  );
+}
